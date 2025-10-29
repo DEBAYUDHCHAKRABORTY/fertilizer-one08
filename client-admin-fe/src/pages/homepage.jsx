1e080f2f35bf2b8ca3ed3cc604a10890d77368ed
Pankaj-web861 <pankajpramanik002@gmail.com>
2025-10-29 09:04:26 +0530

    css added

--- client-admin-fe/src/pages/homepage.jsx
+++ client-admin-fe/src/pages/homepage.jsx
@@ -5,18 +5,18 @@ import { Button } from "primereact/button";
 export default function AdminPage() {
   return (
     <>
-      <div style={{backgroundColor:'#EEEEEE' , padding:"20px" , borderRadius:'20px'}}>
-        
-          <h1 style={{color:'black'}}>
-            <u>Fertilizer Admin Panel</u>
-          </h1>
-        
+      <div style={{ backgroundColor: '#e4e8ddff', padding: "20px", borderRadius: '20px' }}>
+
+        <h1 style={{ color: 'black' }}>
+          <u>Fertilizer Admin Panel</u>
+        </h1>
+
         <br />
-        <div style={{ }}>
+        <div style={{}}>
           <Button
             icon="pi pi-plus"
             label="New QR Code"
-            style={{ marginLeft: "20px", borderColor: "black" , backgroundColor:'white'}}
+            style={{ marginLeft: "20px", borderColor: "black", backgroundColor: 'white' }}
           />
         </div>
         <div
@@ -27,12 +27,12 @@ export default function AdminPage() {
           <InputText
             type="text"
             placeholder="Enter SL NO."
-            style={{ color: "black", width: "300px" , borderColor:''}}
+            style={{ color: "black", width: "300px", borderColor: '' }}
           />
           <Button
             icon="pi pi-search"
             label="Search"
-            style={{ color: "black", borderColor: "black" , backgroundColor:'white'}}
+            style={{ color: "black", borderColor: "black", backgroundColor: 'white' }}
           />
         </div>
       </div>
