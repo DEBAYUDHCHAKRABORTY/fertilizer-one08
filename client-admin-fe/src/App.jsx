4382da57c609038e863f2b9e64d8ef00f927b1d2
Nabin94 <Nabinkrmisra94@gmail.com>
2025-10-30 09:03:36 +0530

    conflict resolved

--- client-admin-fe/src/App.jsx
+++ client-admin-fe/src/App.jsx
@@ -1,28 +1,20 @@
 import './App.css'
-<<<<<<< HEAD
 import ProjectAdmin from './pages/create';
 // import AdminPage from './pages/homepage'
 import { Route, Routes } from "react-router-dom";
-=======
-import DemoDetail from './pages/afterSearch'
-import AdminPage from './pages/homepage'
->>>>>>> main
 
 function App() {
 
   return (
     <>
-<<<<<<< HEAD
       {/* <AdminPage /> */}
       <Routes>
         <Route path="/create" element={<ProjectAdmin />} />
 
       </Routes>
 
-=======
       <AdminPage/>
       <DemoDetail/>
->>>>>>> main
     </>
   );
 }
